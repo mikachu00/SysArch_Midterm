37357d361c240cb64627f7927e90a64ce5469840
mikachu00 <maykylacabanog@gmail.com>
2025-03-24 22:57:57 +0800

    Add project files.

---
+++ Connection/Connection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SysArch_Midterm.Connection
+{
+    internal class Connection
+    {
+        public static OleDbConnection conn;
+        public static string dbconnect = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.StartupPath + "\\SysArch.accdb";
+
+        public static void DB()
+        {
+            try
+            {
+                conn = new OleDbConnection(dbconnect);
+                conn.Open();
+
+
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
